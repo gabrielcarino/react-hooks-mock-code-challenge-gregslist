f35ece2082242bbdead1879a0baa3def24927c6e
gabriel Carino <gabejcarino@gmail.com>
2022-05-11 17:39:13 -0500

    commit tree 9dbe4b39b23f83a312ef87123b6ad3e0c3c95664

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [updatedListings, setUpdatedListiings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(listingsArray => {
+        setListings(listingsArray)
+        setUpdatedListiings(listingsArray)
+      })
+  }, []);
+
+  function handleSearch(search) {
+    const searchListings = listings.filter(listing => listing.description.toLowerCase().includes(search.toLowerCase()))
+    if (search !== "") setUpdatedListiings(searchListings)
+    else setUpdatedListiings(listings)
+  };
+
+  function handleDelete(id) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        const updatedListings = listings.filter(listing => listing.id !== id)
+        setUpdatedListiings(updatedListings)
+      })
+  };
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch}/>
+      <ListingsContainer listings={updatedListings} onDelete={handleDelete} />
     </div>
   );
 }
